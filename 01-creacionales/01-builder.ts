73a2cf94f04e8725e241705b527e42bc621a4200
Nicolás Fuenzalida Contreras <na.fuenzalida.contreras@gmail.com>
2025-01-10 17:52:25 -0300

    patron builder

--- 01-creacionales/01-builder.ts
+++ 01-creacionales/01-builder.ts
@@ -12,3 +12,80 @@
  *
  * https://refactoring.guru/es/design-patterns/builder
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+class Computer {
+  public cpu: string = "cpu - not defined";
+  public ram: string = "ram - not defined";
+  public storage: string = "storage - not defined";
+  public gpu?: string;
+
+  displayConfiguration() {
+    console.log(`Configuración de la computadora
+            CPU: ${this.cpu}
+            RAM: ${this.ram}
+            Alamacenamiento: ${this.storage}
+            GPU: ${this.gpu}
+            `);
+  }
+}
+
+class ComputerBuilder {
+  private computer: Computer;
+
+  constructor() {
+    this.computer = new Computer();
+  }
+
+  setCPU(cpu: string): ComputerBuilder {
+    this.computer.cpu = cpu;
+
+    return this;
+  }
+
+  setRAM(ram: string): ComputerBuilder {
+    this.computer.ram = ram;
+
+    return this;
+  }
+
+  setStorage(storage: string): ComputerBuilder {
+    this.computer.storage = storage;
+
+    return this;
+  }
+
+  setGPU(gpu: string): ComputerBuilder {
+    this.computer.gpu = gpu;
+
+    return this;
+  }
+
+  build() {
+    return this.computer;
+  }
+}
+
+function main() {
+  const basicComputer: Computer = new ComputerBuilder()
+    .setCPU("Intel Core 2 Dúo")
+    .setRAM("4GB")
+    .setStorage("256GB")
+    .build();
+
+  console.log("%cComputadora básica: ", COLORS.blue);
+  basicComputer.displayConfiguration();
+
+  const gamerComputer: Computer = new ComputerBuilder()
+    .setCPU("Intel i9")
+    .setRAM("32GB")
+    .setStorage("1024TB")
+    .setGPU("Nvidia RTX 5090")
+    .build();
+
+  console.log("%cComputadora gamer: ", COLORS.red);
+  gamerComputer.displayConfiguration();
+}
+
+main();
