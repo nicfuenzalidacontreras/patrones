f50a5716e234c979d5151131e73ff891499daebd
Nicolás Fuenzalida Contreras <na.fuenzalida.contreras@gmail.com>
2025-01-19 12:17:47 -0300

    patron factory function

--- 01-creacionales/07-factory-function.ts
+++ 01-creacionales/07-factory-function.ts
@@ -7,3 +7,31 @@
  * * es decir, en tiempo de ejecución y no en tiempo de compilación.
  *
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+type Language = "es" | "en" | "fr";
+
+function createGreeter(lang: Language) {
+  return function (name: string) {
+    const messages = {
+      es: `Hola, %c${name}!`,
+      en: `Hello, %c${name}!`,
+      fr: `Bonjour, %c${name}!`,
+    };
+
+    return console.log(messages[lang], COLORS.red);
+  };
+}
+
+function main() {
+  const spanishGreeter = createGreeter("es");
+  const englishGreeter = createGreeter("es");
+  const frenchGreeter = createGreeter("fr");
+
+  spanishGreeter("Nicolás");
+  englishGreeter("Alice");
+  frenchGreeter("Pierre");
+}
+
+main();
