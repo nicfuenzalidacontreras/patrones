c05346932f55acc84ebf3c6e7290360e15ec86ac
Nicolás Fuenzalida Contreras <na.fuenzalida.contreras@gmail.com>
2025-01-21 16:50:03 -0300

    patron adapter

--- 02-estructurales/01-adapter.ts
+++ 02-estructurales/01-adapter.ts
@@ -10,3 +10,11 @@
  *
  * https://refactoring.guru/es/design-patterns/adapter
  */
+//import { LocalLogger } from "./adapter-files/local-logger.ts";
+import { DenoLoggerAdapter } from "./adapter-files/logger-adapter.ts";
+
+const logger = new DenoLoggerAdapter("01-adapter.ts");
+
+logger.writeLog("Mensaje de un log normal");
+logger.writeWarning("Una alerta normal, información");
+logger.writeError("Algo muy malo salió por aquí");
